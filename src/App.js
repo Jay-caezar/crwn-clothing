9036a00a0bd10e9a7cfd98ca2729458be16409da
jay-caezar <okohjulius24@gmail.com>
2024-05-15 16:43:04 +0100

    trying to resolve

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./category.styles.scss";
 
-function App() {
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+      // imageUrl:'https://images.unsplash.com/photo-1593642634000-888888888888?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      id: 2,
+      title: "Jackets",
+      // imageUrl:'https://images.unsplash.com/photo-1593642634000-888888888888?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      // imageUrl:'https://images.unsplash.com/photo-1593642634000-888888888888?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      id: 4,
+      title: "Women",
+      // imageUrl:'https://images.unsplash.com/photo-1593642634000-888888888888?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      id: 5,
+      title: "Men",
+      // imageUrl:'https://images.unsplash.com/photo-1593642634000-888888888888?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    },
+  ];
   return (
     <div className="App">
       <header className="App-header">
